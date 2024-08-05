e45d62f4312d77a22ae1c1c10f671217106816e3
Nigel Williams-Lucas <4689066+nwlucas@users.noreply.github.com>
2024-08-05 19:31:43 -0400

    Setting up the windows

--- src/routes/(app)/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<div class="container mx-auto grid grid-cols-1 xl:grid-cols-[200px_minmax(0px,_ifr)_200px]">
-	<!-- Sidebar (Left) -->
-	<!-- NOTE: hidden in smaller screen sizes -->
-	<aside class="sticky top-0 col-span-1 hidden h-screen p-4 xl:block"></aside>
-	<!-- Main -->
-	<main class="col-span-1 space-y-4 p-4">
-		<slot />
-	</main>
-</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,36 @@
 <script lang="ts">
 	import '../app.css';
+	import { AppBar, AppRail, autoModeWatcher } from '@skeletonlabs/skeleton';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		autoModeWatcher();
+	});
 </script>
 
-<div class="grid grid-rows-[auto_1fr_auto]">
+<div class="grid h-screen grid-rows-[auto_1fr_auto]">
 	<!-- Header -->
-	<header></header>
-	<!-- Page -->
-	<slot />
-	<!-- Footer -->
-	<footer class="p-4"></footer>
+	<header>
+		<AppBar
+			gridColumns="grid-cols-3"
+			slotDefault="place-self-center"
+			slotTrail="place-content-end"
+			padding="p-4"
+		>
+			<svelte:fragment slot="lead">(actions)</svelte:fragment>
+			SSCAN
+			<svelte:fragment slot="trail">(actions)</svelte:fragment>
+		</AppBar>
+	</header>
+	<!-- Grid Columns -->
+	<div class="grid grid-cols-1 md:grid-cols-[auto_1fr]">
+		<!-- Left Sidebar -->
+		<aside class="p-4">
+			<AppRail></AppRail>
+		</aside>
+		<!-- Page -->
+		<main class="space-y-4 p-4">
+			<slot />
+		</main>
+	</div>
 </div>
