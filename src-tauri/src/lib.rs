ec54b140b48190b5a1d3ca7636c9a30a510b9fcc
Nigel Williams-Lucas <4689066+nwlucas@users.noreply.github.com>
2024-09-06 18:46:06 -0400

    Starting to initialize the on system database

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -15,6 +15,7 @@ pub fn run() {
     }];
 
     tauri::Builder::default()
+        .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(
             tauri_plugin_sql::Builder::default()
