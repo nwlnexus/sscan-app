5e696eef39783b36d66c4a45afa0ec618c4e0a87
Nigel Williams-Lucas <4689066+nwlucas@users.noreply.github.com>
2024-09-10 20:57:39 -0400

    correcting some derivations

--- seeds/admin-profile.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO "profile" (display_name, email, email_verified, password_hash, is_admin, created_at)
-VALUES (
-  'Admin',
-  'admin@sscan.app',
-  CURRENT_TIMESTAMP,
-  'dc9c4727304ee85575a99e814cdb4900:b72a67c348a756c3761157bcf846a096e7f4695f85fe1dc03525992bce52dddd',
-  true,
-  CURRENT_TIMESTAMP
-);
