aebace9ff2858921d6a6971127fe3ed7496d98a3
Nigel Williams-Lucas <nwilliams-lucas@nwl-mbm2.raptor-mimosa.ts.net>
2024-09-24 00:55:05 -0400

    Setting up the action

---
+++ templates/act.tpl
@@ -0,0 +1,1 @@
+OP_SERVICE_ACCOUNT_TOKEN="{{op://Dev/6kaioohr3osgdar7hm5wimy3je/credential}}"
---
+++ tooling/cf_pages_secrets/eslint.config.js
@@ -0,0 +1,5 @@
+// @ts-expect-error Unsure why tbh
+import eslintConfig from '@nwlnexus/config/eslint'
+
+/** @type {import('eslint').Linter.Config[]} */
+export default eslintConfig
